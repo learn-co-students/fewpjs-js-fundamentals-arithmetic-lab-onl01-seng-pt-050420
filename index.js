a7310bb675f0e62503be67a06db1fa44af9809fd
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-19 02:49:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.random() + 1;
+
+let mod = 19 % 5;
+
+let max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
