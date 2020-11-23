f760d5c33471cfe82cd4ac03649adfd630fe9ee1
mohehexd <dumblemoe@gmail.com>
2020-11-23 15:30:11 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let mod = 16 % 6 ;
+console.log(mod);
+
+const max = Math.max(10, 20);
+console.log(max);
